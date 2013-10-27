5217e0bc7d79a04ed3352274048f9b83b98162aa
GOGEN <gogen.marat@gmail.com>
2013-10-27 20:55:14 +0400

    Numerovka

--- JavaPublisher.java
+++ JavaPublisher.java
@@ -2,6 +2,7 @@ public class JavaPublisher{
 	public static void main(String[] args){
 		String[] names = new String[]{"Foma Akvinsky", "Alexandr Makedonsky", "Iosif Brodsky"};
 		for(int i=0;i<names.length;i++){
+			System.out.println(i+")");
 			System.out.print(names[i]+", ");
 		}
 	}
