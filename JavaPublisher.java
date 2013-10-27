f4e3073a55d8cda8f3922662254677da3d5dc496
GOGEN <gogen.marat@gmail.com>
2013-10-27 20:06:09 +0400

    MyNames

--- JavaPublisher.java
+++ JavaPublisher.java
@@ -1,6 +1,12 @@
 public class JavaPublisher{
 	public static void main(String[] args){
 		String[] names = new String[]{"Foma Akvinsky", "Alexandr Makedonsky", "Iosif Brodsky"};
+		String[] namesbuf=new String[names.length+1];
+		for(int i=0;i<names.length;i++){
+			namesbuf[i]=names[i];
+		}
+		namesbuf[names.length]="Marat Latypov";
+		names=namesbuf;
 		for(int i=0;i<names.length;i++){
 			System.out.print(names[i]+", ");
 		}
