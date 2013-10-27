6b7e1262fec9cedb70eb5649194430028771231d
GOGEN <gogen.marat@gmail.com>
2013-10-27 19:32:15 +0400

    Modify

--- JavaPublisher.java
+++ JavaPublisher.java
@@ -1,3 +1,9 @@
-public class NameArray{
-	String[] names = new String[]{"Foma Akvinsky", "Alexandr Makedonsky", "Iosif Brodsky"};
+public class JavaPublisher{
+	public static void main(String[] args){
+		String[] names = new String[]{"Foma Akvinsky", "Alexandr Makedonsky", "Iosif Brodsky"};
+		for(int i=0;i<names.length;i++){
+			System.out.print(names[i]+", ");
+		}
+	}
+	
 }
